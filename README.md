ee039bf4d586e685c6dbd805bc0e95158b539436
Fil <fillip@ignitepixels.com>
2013-11-15 19:01:23 -0800

    Small final tweaks to script/demo page. Finishing up documentation.

--- README.md
+++ README.md
@@ -1,7 +1,56 @@
-Jquery-Sidebar-Page-Navigation
-==============================
+# Jquery Sidebar Page Navigation
 
 A Jquery plugin that adds a sidebar of links to jump to certain parts of a page.
 
+## Features
+- Doesn't require extra markup (pure JS)
+- Options for customizable classes/HTML Wrapper, animation speed, and more
 
-**Coming Soon!!**
+## Requirements
+- Jquery
+- CSS to style navigation
+
+## Q/A
+Q) Why did you choose to inject HTML through javascript instead of having the developer add HTML to the page themselves?
+A) I'm a huge fan of not cluttering a page with unnecessary HTML markup, so I opted to inject the HTML by javascript. There is options to customize the markup/classes this plugin uses. This way, your code is easier to maintain.
+
+Q) Why isn't this plugin responsive?!
+A) When I use this plugin, I will opt to hide the sidebar on smaller screens. This is because there is already a limited amount of space in small screens, so having another navigation would not be ideal. If you have a suggestion of how to make this navigation work at smaller screens, send a pull request =]
+	
+
+## Options
+```javascript
+$.pageNavigation({
+	navigationTag : 'nav',
+	// HTML Tag to wrap entire navigation in
+	// Accepts: String
+	// Default: 'nav'
+
+	navigationClass : 'pageNavigation',
+	// Class to give navigation
+	// Accepts: String
+	// Default: 'pageNavigation'
+
+
+	scrollToTriggerClass : 'scrollToTrigger',
+	// Class to give navigation items
+	// Accepts: String
+	// Default: 'scrollToTrigger'
+
+
+	navigationTopPadding : 200,
+	// Top padding on Navigation
+	// Accepts: Integer
+	// Default: 200
+
+	pageNavigationTargets : '.pageNavigationTarget',
+	// Sections to be added to navigation items
+	// Accepts: Jquery Selector or String
+	// Default: '.pageNavigationTarget'
+
+	navigationAnimationSpeed : 500,
+	// Speed of navigation animation in milliseconds
+	// Accepts: Integer
+	// Default: 500
+});
+```
--- sidebarPageNavigation.js
+++ sidebarPageNavigation.js
@@ -1,74 +1,69 @@
 /* Jquery SidebarPageNavigation */
 (function($){
 
-    $.pageNavigation = {
-        defaultOptions : {
-            navigationClass : 'pageNavigation',
-            scrollToTriggerClass : 'scrollToTrigger',
-            navigationTopPadding : 200,
-            pageNavigationTargets : '.pageNavigationTarget',
-        }
-    }
+    $.pageNavigation = function(newOptions){
+        
+        var options = $.extend({
+                navigationTag : 'nav',
+                navigationClass : 'pageNavigation',
+                scrollToTriggerClass : 'scrollToTrigger',
+                navigationTopPadding : 200,
+                pageNavigationTargets : '.pageNavigationTarget',
+                navigationAnimationSpeed : 500,
+            }, newOptions),
 
-    $.extend({
-        pageNavigation : function(newOptions){
-            var options = $.extend($.pageNavigation.defaultOptions, newOptions),
-                $window = $(window),
-                pageNavigationTargets = $(options.pageNavigationTargets),
-                navigation,
-                sidebarHTML,
-                navigationOffset,
+            $window = $(window),
+            pageNavigationTargets = $(options.pageNavigationTargets),
+            navigation,
+            sidebarHTML,
+            navigationOffset,
 
-                /*
-                    To Do:
-                    - Check over code to insure it works in Jquery Plugin state
-                    - Add CSS for different sidebar styles
-                */
+            init = function(){
+                if (pageNavigationTargets.length){
+                    sidebarHTML =  '<' + options.navigationTag + ' class="' + options.navigationClass + '">';
+                    sidebarHTML += '<ul>';
 
-                init = function(){
-                    // Create Page Navigation
-                    if (pageNavigationTargets.length){
-                        sidebarHTML =  '<nav class="' + options.navigationClass + '">';
-                        sidebarHTML += '<ul>';
+                    pageNavigationTargets.each(function(idx){
+                        $this = $(this);
+                        sidebarHTML += '<li><a href="#' + $this.attr('id') + '" data-section-title="' + $this.attr('data-section-title') + '" class="' + options.scrollToTriggerClass + '"></a></li>';
+                    });
 
-                        pageNavigationTargets.each(function(idx){
-                            $this = $(this);
-                            sidebarHTML += '<li><a href="#' + $this.attr('id') + '" data-section-title="' + $this.attr('data-section-title') + '" class="' + options.scrollToTriggerClass + '"></a></li>';
-                        });
-
-                        sidebarHTML += '</ul>';
-                        sidebarHTML += '</nav>';
-                        $('body').append(sidebarHTML);
-                        navigation = $('.' + options.navigationClass);
-                    }
+                    sidebarHTML += '</ul>';
+                    sidebarHTML += '</' + options.navigationTag + '>';
+                    $('body').append(sidebarHTML);
+                    navigation = $('.' + options.navigationClass);
 
                     // Bind Scroll
                     navigationOffset = navigation.offset();
                     $window.on('scroll resize', positionNavigation);
-                },
-
-                positionNavigation = function(){
-                    navigation.stop().animate({ top: ($window.scrollTop() + navigationTopPadding > navigationTopPadding) ? $window.scrollTop() + navigationTopPadding : navigationTopPadding }, options.navigationAnimationSpeed);
-                },
-
-                scrollTo = function(){
-                    var scrollToTriggers = $('.' + options.scrollToTriggerClass);
-
-                    scrollToTriggers.on('click', function(e){
-                        var target = $($(this).attr('href'));
-
-                        e.preventDefault();
-
-                        if( target.length ){
-                            $('html, body').animate({
-                                scrollTop: target.offset().top
-                            }, options.navigationAnimationSpeed);
-                        }
-                    });
                 }
 
-                init();
-        }
-    });
+                positionNavigation();
+                scrollToInit();
+
+            },
+
+            positionNavigation = function(){
+                navigation.stop().animate({ top: ($window.scrollTop() + options.navigationTopPadding > options.navigationTopPadding) ? $window.scrollTop() + options.navigationTopPadding : options.navigationTopPadding }, options.navigationAnimationSpeed);
+            },
+
+            scrollToInit = function(){
+                var scrollToTriggers = $('.' + options.scrollToTriggerClass);
+
+                scrollToTriggers.on('click', function(e){
+                    var target = $($(this).attr('href'));
+
+                    e.preventDefault();
+
+                    if( target.length ){
+                        $('html, body').animate({
+                            scrollTop: target.offset().top
+                        }, options.navigationAnimationSpeed);
+                    }
+                });
+            }
+
+            init();
+    }
 
 })(jQuery);
